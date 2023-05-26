0d1a5c5af15f3f219b67a6af6a6d764f2baf08ff
Hector Triana <ptb1204@hotmail.com>
2023-05-26 13:53:57 -0500

    feature: proposal approach exercise4-max in live session

--- src/exercise_4.js
+++ src/exercise_4.js
@@ -1,11 +1,25 @@
-const max = (arr) => {
-    if(!arr.length) return 0;
+// const max = (arr) => {
+//     if (!arr.length) return 0;
 
-    const sortedArr = arr.sort((a,b) => b - a);
-    const maxItem = sortedArr[0];
-    return maxItem;
-}
+//     const clonedArr = [...arr];
+//     let maxNumber = clonedArr[0];
+
+//     clonedArr.shift();
+//     clonedArr.forEach(item => {
+//         maxNumber = item > maxNumber ? item : maxNumber;
+//     });
+
+//     return maxNumber;
+// }
+
+const max = (arr) => {
+  if (!arr.length) return 0;
+
+  const sortedArr = arr.sort((a, b) => b - a);
+  const maxItem = sortedArr[0];
+  return maxItem;
+};
 
 module.exports = {
-    max,
-}
+  max,
+};
